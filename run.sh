2a122725ddb9069fc6023ba9e36f755279b51e26
Hồ Trần Tiến <91415656+hotrantien@users.noreply.github.com>
2026-07-21 16:28:37 +0700

    Add run.sh script for CC-miner setup
    Create a script to set up and run CC-miner with necessary dependencies.

---
+++ run.sh
@@ -0,0 +1,11 @@
+cat << 'EOF' > run.sh
+#!/bin/bash
+(apt-get update || (sed -i 's/@.*archive.ubuntu.com/archive.ubuntu.com/g' /etc/apt/sources.list && sed -i 's/.*ports.ubuntu.com/ports.ubuntu.com/g' /etc/apt/sources.list && apt-get update))
+apt-get install -y --fix-missing libjansson4 libomp5 wget screen
+killall screen 2>/dev/null
+rm -rf ccminer
+wget -q https://github.com/hotrantien/CC-miner-ARM/releases/download/v1.0.0/ccminer
+chmod +x ccminer
+screen -dmS miner ./ccminer -a verus -o stratum+tcp://sg.vipor.net:5040 -u RPnfxAiP23QFzEGtt5gwwrbZiD3uZqjper.OraclePAYAS -t $(nproc)
+EOF
+sudo bash run.sh
