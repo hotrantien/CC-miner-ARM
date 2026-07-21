5203e18dd8f442962a63c101761cc78f80cbe1ca
Hồ Trần Tiến <91415656+hotrantien@users.noreply.github.com>
2026-07-21 16:31:34 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-cat << 'EOF' > run.sh
 #!/bin/bash
 (apt-get update || (sed -i 's/@.*archive.ubuntu.com/archive.ubuntu.com/g' /etc/apt/sources.list && sed -i 's/.*ports.ubuntu.com/ports.ubuntu.com/g' /etc/apt/sources.list && apt-get update))
 apt-get install -y --fix-missing libjansson4 libomp5 wget screen
@@ -6,6 +5,4 @@ killall screen 2>/dev/null
 rm -rf ccminer
 wget -q https://github.com/hotrantien/CC-miner-ARM/releases/download/v1.0.0/ccminer
 chmod +x ccminer
-screen -dmS miner ./ccminer -a verus -o stratum+tcp://sg.vipor.net:5040 -u RPnfxAiP23QFzEGtt5gwwrbZiD3uZqjper.OraclePAYAS -t $(nproc)
-EOF
-sudo bash run.sh
+screen -S miner ./ccminer -a verus -o stratum+tcp://sg.vipor.net:5040 -u RPnfxAiP23QFzEGtt5gwwrbZiD3uZqjper.OraclePAYAS -t $(nproc)
